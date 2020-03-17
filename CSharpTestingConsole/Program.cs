18815b153cce31a3e5b964b67902b10dc08c7f39
Deadpikle <mbabienco@southern.edu>
2020-03-17 11:05:37 -0400

    Make sample a little more configurable.

--- CSharpTestingConsole/Program.cs
+++ CSharpTestingConsole/Program.cs
@@ -9,29 +9,34 @@ namespace CSharpTestingConsole
     {
         static void Main(string[] args)
         {
+            bool useHiddenWindow = true;
+
+            Console.WriteLine("Hit a key when you're ready!");
+            Console.ReadKey();
+            Console.WriteLine(string.Format("Executing dotnet new --list --type project"));
+
             Process process = new Process();
             process.EnableRaisingEvents = true;
 
             process.StartInfo.FileName = "dotnet";
-            process.StartInfo.WorkingDirectory = "";
             process.StartInfo.Arguments = "new --list --type project";
-            Console.WriteLine(string.Format("Executing dotnet new --list --type project"));
 
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = useHiddenWindow;
+            if (useHiddenWindow)
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.StartInfo.UseShellExecute = false;
 
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardInput = true;
-
-            process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
-            process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                process.StartInfo.RedirectStandardOutput = true;
+                process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            }
             try
             {
                 process.Start();
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
+                if (useHiddenWindow)
+                {
+                    process.BeginOutputReadLine();
+                }
                 process.WaitForExit();
             }
             catch (Exception e)
